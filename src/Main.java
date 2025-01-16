ba2f5ad6713919bf3f3f6dea5eaa02e5be63dc8c
Martazyy <martazyy@vk.com>
2025-01-16 10:38:43 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите число: ");
-        int num = in.nextInt();
-        in.close();
 
-        if ((num % 2) == 0){
-            System.out.printf("Число " + num + " четное");
         }
-        else System.out.printf("Число " + num + " нечетное");
 
     }
 }
