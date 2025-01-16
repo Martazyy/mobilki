45bee2921160af99c9d6d4c8a17b58cdc53eb08a
Martazyy <martazyy@vk.com>
2025-01-16 12:33:09 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
 
-    public static void main(String[] args) {
-
-        }
-
+    public static void main(String args[]){
+        System.out.println("ХУЙ");
     }
 }
